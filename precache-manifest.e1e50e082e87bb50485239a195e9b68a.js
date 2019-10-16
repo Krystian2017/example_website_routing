9c90290026f3562c732eb1bb9fd076f28cb485d6
Krystian Bieniek <krystekk2013@gmail.com>
2019-10-16 14:32:26 +0200

    Updates

---
+++ precache-manifest.e1e50e082e87bb50485239a195e9b68a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ce5f62f1291a454d2f96a54bbc6772",
+    "url": "/example_website_routing/index.html"
+  },
+  {
+    "revision": "d726c1b40852e84f6f3e",
+    "url": "/example_website_routing/static/css/main.46280e49.chunk.css"
+  },
+  {
+    "revision": "a874cc2d7ae27f71b7a2",
+    "url": "/example_website_routing/static/js/2.36a279e1.chunk.js"
+  },
+  {
+    "revision": "d726c1b40852e84f6f3e",
+    "url": "/example_website_routing/static/js/main.2a34759c.chunk.js"
+  },
+  {
+    "revision": "d1c9f81e87fafb7a1c41",
+    "url": "/example_website_routing/static/js/runtime-main.188ed2d4.js"
+  },
+  {
+    "revision": "d77aa897e45307c7f8cb77b653960c4c",
+    "url": "/example_website_routing/static/media/header1.d77aa897.jpg"
+  },
+  {
+    "revision": "e0236b915021fdbab21cca3d6ecb33a3",
+    "url": "/example_website_routing/static/media/header2.e0236b91.jpg"
+  },
+  {
+    "revision": "0fcc502c765bc9450427aa369f19276e",
+    "url": "/example_website_routing/static/media/header3.0fcc502c.jpg"
+  }
+]);
